286ef5be6c4f12342f63a8e295e79d21f8e87b76
Benno Joy <benno@ansible.com>
2015-05-20 18:13:57 +0530

    azure vnet final

--- module/azure.py
+++ module/azure.py
@@ -1,28 +1,38 @@
 import requests
 import json
 import sys
+import time
 
 
 def azure_common_argument_spec():
     return dict(
         azure_password=dict(aliases=['password'], no_log=True),
         azure_username=dict(aliases=['username']),
+        client_id           = dict(required=False, default=None),
+        client_secret       = dict(required=False, default=None, no_log=True),
     )
 
 class AzureConn():
     url = None
     resource = None
     token =  None
-    def __init__(self, url=None, resource=None, user=None, password=None, client_id=None, module=None):
+    def __init__(self, url=None, resource=None, user=None, password=None, client_id=None, client_secret=None, module=None):
         if url:
             self.url = url
         else:
             self.url = 'https://login.windows.net/common/oauth2/token'
+        if not client_id:
+            #The xplat client id
+            client_id = '04b07795-8ddb-461a-bbee-02f9e1bf7b46'
         if resource:
             self.resource = resource
         else:
             self.resource = 'https://management.azure.com/'
-        payload = { 'resource': self.resource, 'username': user, 'password': password, 'client_id': client_id, 'grant_type': 'password' }
+        if user and password:
+            grant_type = 'password'
+        if client_id and client_secret:
+            grant_type = 'client_credentials'
+        payload = { 'resource': self.resource, 'username': user, 'password': password, 'client_id': client_id, 'grant_type': grant_type }
         self.module = module
         token_dict = {}
         try:
@@ -38,6 +48,14 @@ class AzureConn():
         header_dict = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + self.token}
         try:
             resp = requests.get( url, headers = header_dict)
+        except requests.exceptions.RequestException as e:
+            self.module_fail_json(msg="error connecting to azure endpoint for get operation for url %s" %url)
+        return resp
+    
+    def az_delete(self, url):
+        header_dict = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + self.token}
+        try:
+            resp = requests.delete( url, headers = header_dict)
         except requests.exceptions.RequestException as e:
             self.module_fail_json(msg="error connecting to azure endpoint for get operation for url %s" %url)
         return resp
@@ -57,6 +75,73 @@ class AzureConn():
         except requests.exceptions.RequestException as e:
             self.module_fail_json(msg="error connecting to azure endpoint for put operation for url %s" %url)
         return resp
+
+def create_resource(conn, url=None, data=None, wait_timeout=None, module=None):
+    resp = conn.az_put(url, data)
+    if resp.status_code == 202:
+        if wait_timeout == 0:
+            module.exit_json(changed=True, msg="The request for creating the resource has been submitted")
+        resource_status(conn, url, 'create', wait_timeout, module)
+    else:
+        resp_data =  json.loads(resp.content)
+        if 'error' in resp_data.keys():
+            module.fail_json(msg=resp_data)
+
+def delete_resource(conn, url=None, wait_timeout=None, module=None):
+    resp = conn.az_delete(url)
+    if resp.status_code == 202:
+        if wait_timeout == 0:
+            module.exit_json(changed=True, msg="The request for Deleteing the resource has been submitted")
+        resource_status(conn, url, 'delete', wait_timeout, module)
+    else:
+        resp_data =  json.loads(resp.content)
+        if 'error' in resp_data.keys():
+            module.fail_json(msg=resp_data)
+
+def resource_status(conn=None, url=None, status=None, wait_timeout=None, module=None):
+    if status == 'present':
+        resp = conn.az_get(url)
+        resp_data =  json.loads(resp.content)
+        if 'error' in resp_data.keys():
+            if resp_data['error']['code'] == 'ResourceNotFound':
+                return False
+            else:
+                module.fail_json(msg=resp_data)
+        if resp.status_code == 200:
+            return True
+        return False
+    if status == 'get_json':
+        resp = conn.az_get(url)
+        resp_data =  json.loads(resp.content)
+        if 'error' in resp_data.keys():
+            module.fail_json(msg=resp_data)
+        return resp.json()
+    if status == 'create':
+        timeout = time.time() + wait_timeout
+        time.sleep(6)
+        while timeout > time.time():
+            resp = conn.az_get(url)
+            resp_data =  json.loads(resp.content)
+            if 'error' in resp_data.keys():
+                module.fail_json(msg=resp_data)
+            if resp_data['properties']['status'] == 'Created':
+                module.exit_json(changed=True, msg=resp_data)
+            time.sleep(5)
+        module.fail_json(msg="timed out waiting for resource to be created")
+    if status == 'delete':
+        timeout = time.time() + wait_timeout
+        time.sleep(1)
+        while timeout > time.time():
+            resp = conn.az_get(url)
+            resp_data =  json.loads(resp.content)
+            if 'error' in resp_data.keys():
+                if resp_data['error']['code'].find('NotFound') > 0:
+                    module.exit_json(changed=True, msg="Resource Deleted")
+                else:
+                    module.fail_json(msg=resp_data)
+            time.sleep(5)
+        module.fail_json(msg="timed out waiting for resource to be Deleted")
+
 
 class Template(object):
     props = {
--- module/azure_arm_vnet.py
+++ module/azure_arm_vnet.py
@@ -1,19 +1,179 @@
 #!/usr/bin/python
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+DOCUMENTATION = '''
+---
+module: azure_arm_vnet
+short_description: create, remove Virtual networks in Azure via Azure Resource Manager
+description:
+    - Creates or removes Virtual Networks.
+version_added: "2.0"
+options:
+  username:
+    description:
+      - The email/username of the azure account
+    required: false
+    default: null
+  password:
+    description:
+      - Password to the azure account
+    required: false
+    default: null
+  client_id:
+    description:
+      - client_id if you are using application authentication, mutually exclusive with username.
+    required: false
+    default: null
+  client_secret:
+    description:
+      - client_secret, if you are using application authentication, mutually exclusive with password.
+    required: false
+    default: null
+  name:
+    description:
+      - The name of the virtual network to be created
+    required: false
+    default: null
+  resource_group:
+    description:
+      - The name of the resource group where the virtual network is to be created
+    required: True
+  location:
+    description:
+      - The location where the virtual network is to be created
+    required: False
+    default: 'eastasia'  
+  sub_id:
+    description:
+      - The subscription id  where the virtual network is to be created
+    required: true
+  state:
+    description:
+      - The state of the resource present/absent
+    required: False
+    default: 'present'   
+  wait:
+    description:
+      - wether to wait for state of the resourse to become present/absent
+    required: false
+    default: 'yes'  
+  wait_timeout:
+    description:
+      - The time in seconds for the  state of the resourse to become present/absent
+    required: false
+    default: 480
+  s_s_vpn:
+    description:
+      - The Site to Site vpn configuration, see Example for usage
+    required: false
+    default: null
+  address_spaces:
+    description:
+      - The main addressspaces to be used for this Virtual Network, see Example for usage
+    required: false
+    default: "10.0.0.0/16"
+  subnets:
+    description:
+      - The subnets to be created for this Virtual Network, see Example for usage
+    required: false
+    default: "10.0.1.0/24"
+  dns_servers:
+    description:
+      - The dns servers to be assigned  for the machines provisioned in thsi  Virtual Network, see Example for usage
+    required: false
+    default: null
+
+author: benno@ansible.com
+'''
+
+EXAMPLES = '''
+# Basic vnet provisioning example
+
+- hosts: all
+  connection: local
+  tasks:
+   - azure_arm_vnet:
+       username: '<email>'
+       password: '<password>'
+       resource_group: bentest
+       sub_id: '3f7e29ba-24e-49a14bda37'
+       name: bennet
+       dns_servers: ["2.2.2.2", "3.3.3.3"]
+       address_spaces:
+            - 192.168.0.0/16
+       subnets:
+         - name: ben1
+           subnet: 192.168.1.0/24
+
+#Vnet with site to site vpn configured
+
+- hosts: all
+  connection: local
+  tasks:
+   - azure_arm_vnet:
+       username: '<email>'
+       password: '<password>'
+       resource_group: bentest
+       sub_id: '3f70-42f6-8d9c-5bda37'
+       name: bennet
+       dns_servers: "2.2.2.2,3.3.3.3"
+       address_spaces:
+            - 192.168.0.0/16
+       subnets:
+         - name: ben1
+           subnet: 192.168.1.0/24
+         - name: GatewaySubnet
+           subnet: 192.168.2.0/24
+       s_s_vpn:
+            site_name: "bentest"
+            address_spaces: ["10.30.0.0/16"]
+            gateway_ip: "3.1.1.1"
+
+
+#Delete a vnet
+
+- hosts: all
+  connection: local
+  tasks:
+   - azure_arm_vnet:
+       username: '<email>'
+       password: '<password>'
+       resource_group: bentest
+       state: 'absent'
+       sub_id: '3f0-42f6-8d9c-bda37'
+       name: bennet
+
+'''
+
 
 import json
-import requests
 import collections
 import time
 
-from ansible.module_utils.azure import *
-
 VALID_SUBNET_KEYS = [ 'name', 'subnet']
+VALID_SVPN_KEYS = [ 'site_name', 'address_spaces', 'gateway_ip']
+
+from ansible.module_utils.azure import *
 
 class Vnet(BaseAzureObject):
     props = {
         'addressSpace': (dict, False),
         'subnets': (list, False),
         'dhcpOptions': (dict, False),
+        'gatewayProfile': (dict, False),
         'vpnClientAddressSpace': (dict, False)
     }
 
@@ -23,60 +183,20 @@ def cmp_dict(dict1, dict2):
     od2 = collections.OrderedDict(sorted(dict2.items()))
     return  od1 == od2
 
-def resource_status(conn=None, url=None, status=None, wait_timeout=None, module=None):
-    if status == 'present':
-        resp = conn.az_get(url)
-        resp_data =  json.loads(resp.content)
-        if 'error' in resp_data.keys():
-            if resp_data['error']['code'] == 'ResourceNotFound':
-                return False
-            else:
-                module.fail_json(msg=resp_data)
-        if resp.status_code == 200:
-            return True
-        return False
-    if status == 'get_json':
-        resp = conn.az_get(url)
-        resp_data =  json.loads(resp.content)
-        if 'error' in resp_data.keys():
-            module.fail_json(msg=resp_data)
-        return resp.json()
-    if status == 'create':
-        timeout = time.time() + wait_timeout
-        time.sleep(1)
-        while timeout > time.time():
-            resp = conn.az_get(url)
-            resp_data =  json.loads(resp.content)
-            if 'error' in resp_data.keys():
-                module.fail_json(msg=resp_data)
-            if resp_data['properties']['status'] == 'Created':
-                module.exitl_json(changed=true, msg=resp_data)
-            time.sleep(5)
-        module.fail_json(msg="timed out waiting for resource to be created")
-
-def create_resource(conn, url=None, data=None, wait_timeout=None, module=None):
-    resp = conn.az_put(url, data)
-    if resp.status_code == 202:
-        if wait_timeout == 0:
-            module.exit_json(changed=True, msg="The request for creating the Vnet has been submitted")
-        resource_status(conn, url, 'create', wait_timeout)
-    else:
-        resp_data =  json.loads(resp.content)
-        if 'error' in resp_data.keys():
-            module.fail_json(msg=resp_data)
-        
-
 def main():
     argument_spec = azure_common_argument_spec()
     argument_spec.update(dict(
-        sub_id          = dict(required=True),
-        resource_group  = dict(required=True),
-        name            = dict(required=False, default='default_vnet'),
-        location        = dict(required=False, default='eastasia'),
-        wait            = dict(required=False, type='bool', default=True),
-        wait_timeout    = dict(required=False, type='int', default=480),
-        address_spaces  = dict(type='list',   required=False, default=['10.0.0.0/16']),
-        subnets         = dict(type='list',   required=False, default=[{'name':'default', 'subnet':'10.0.1.0/24'}]),
+        sub_id              = dict(required=True),
+        resource_group      = dict(required=True),
+        state               = dict(default='present', choices=['present', 'absent']),
+        name                = dict(required=False, default='default_vnet'),
+        location            = dict(required=False, default='eastasia'),
+        wait                = dict(required=False, type='bool', default=True),
+        wait_timeout        = dict(required=False, type='int', default=480),
+        s_s_vpn             = dict(required=False, default=None),
+        address_spaces      = dict(type='list',   required=False, default=['10.0.0.0/16']),
+        subnets             = dict(type='list',   required=False, default=[{'name':'default', 'subnet':'10.0.1.0/24'}]),
+        dns_servers         = dict(type='list',   required=False, default=None),
         )
     ) 
     module = AnsibleModule(
@@ -85,22 +205,40 @@ def main():
     address = []
     subnets = []
 
-    username    = module.params.get('username')
-    password    = module.params.get('password')
-    client_id   = '04b07795-8ddb-461a-bbee-02f9e1bf7b46'
-    sub_id      = module.params.get('sub_id')
-    name        = module.params.get('name')
-    location    = module.params.get('location')
-    address     = module.params.get('address_spaces')
-    subnets     = module.params.get('subnets')
-    res_grp     = module.params.get('resource_group')
-    wait        = module.params.get('wait')
-    wait_tmout  = module.params.get('wait_timeout')
+    username        = module.params.get('username')
+    password        = module.params.get('password')
+    client_id       = module.params.get('client_id')
+    client_secret   = module.params.get('client_id')
+    sub_id          = module.params.get('sub_id')
+    state           = module.params.get('state')
+    name            = module.params.get('name')
+    location        = module.params.get('location')
+    address         = module.params.get('address_spaces')
+    subnets         = module.params.get('subnets')
+    s_vpn           = module.params.get('s_s_vpn')
+    dns             = module.params.get('dns_servers')
+    res_grp         = module.params.get('resource_group')
+    wait            = module.params.get('wait')
+    wait_tmout      = module.params.get('wait_timeout')
     if not wait:
         wait_tmout = 0
-
-    address_space=dict(addressPrefixes=address)
-    put_url = 'https://management.azure.com/subscriptions/' + sub_id + '/resourceGroups/' + res_grp + '/providers/Microsoft.ClassicNetwork/virtualNetworks/' + name + '?api-version=2014-06-01'
+    
+    #The endpoint for vnet api
+    put_url        = 'https://management.azure.com/subscriptions/' + sub_id + '/resourceGroups/' + res_grp + '/providers/Microsoft.ClassicNetwork/virtualNetworks/' + name + '?api-version=2014-06-01'
+    
+    # Make a connection to the azure arm endpoint to get a valid token    
+    conn = AzureConn(user=username, password=password, client_id=client_id, client_secret=client_secret, module=module)
+    
+    #Delete if state is absent
+    if state == 'absent':
+        if not resource_status(conn, put_url, 'present', wait_tmout, module):
+            module.exit_json(changed=False, msg="Resourse does not exist")
+        else:
+            delete_resource(conn, put_url, wait_tmout, module)
+            
+    #Not delete so create or check if it exists
+    address_space  = dict(addressPrefixes=address)
+    dns            = dict(dnsServers=dns)
 
     # Azure expects the subnet to have the key addressPrefix, so lets substitute subnet with addressPrefix
     for idx, val in enumerate(subnets):
@@ -110,19 +248,26 @@ def main():
             if j == 'subnet':
                 subnets[idx]['addressPrefix'] = subnets[idx]['subnet']
                 subnets[idx].pop('subnet', None)
-            
-    # Make a connection to the azure arm endpoint to get a valid token    
-    conn = AzureConn(user=username, password=password, client_id=client_id, module=module)
 
+    #Lets check if site to site values are valid if provided
+    if s_vpn:
+        for i in s_vpn.keys():
+            if i not in VALID_SVPN_KEYS:
+                module.fail_json(msg = "%s is not a valid key for site vpn  param,The valid ones are %s" %(i, VALID_SVPN_KEYS ))
+            if i =='address_spaces' and not isinstance(s_vpn[i], list):
+                module.fail_json(msg="address_spaces in s_vpn param must be a list")
+        gateway = dict(size="Small", localNetworkSites=[dict(localNetworkSiteName=s_vpn['site_name'], addressSpace=s_vpn['address_spaces'], vpnGatewayIpAddress=s_vpn['gateway_ip'], connectionTypes=['IPsec'])])
+    else:
+        gateway = None
     template = Template()
-    net1 = Vnet(name=name, location=location, addressSpace=address_space, subnets=subnets)
+    net1 = Vnet(name=name, location=location, addressSpace=address_space, subnets=subnets, dhcpOptions=dns, gatewayProfile=gateway)
     template.add_resource(net1)
     data = json.loads(template.to_json())
     vnet = data['resources'][0]
     if not resource_status(conn, put_url, 'present', wait_tmout, module):
         create_resource(conn, put_url, json.dumps(vnet), wait_tmout, module)
     else:
-        vnet_json = resource_status(conn, put_url, 'get_json')
+        vnet_json = resource_status(conn, put_url, 'get_json', 0, module)
         vnet_data = json.loads(json.dumps(vnet_json))
         vnet_data.pop('id')
         vnet_data.pop('type')
@@ -132,8 +277,12 @@ def main():
         vnet_data['properties'].pop('inUse')
         vnet_data['properties'].pop('status')
         vnet_current = data['resources'][0]
+        if not dns:
+            vnet_current['properties'].pop('dhcpOptions')
+        if not s_vpn:
+            vnet_current['properties'].pop('gatewayProfile')
         if cmp_dict(vnet_current, vnet_data):
-            res_json  = resource_status(conn, put_url, 'get_json')
+            res_json  = resource_status(conn, put_url, 'get_json', 0, module)
             module.exit_json(changed=False, res=res_json) 
         else:
             create_resource(conn, put_url, json.dumps(vnet), wait_tmout, module)
